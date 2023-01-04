e85ceca6302e62aad57b9d8e6db7c0db72662595
OleksandrNovitskyi <novitskyi.o.s@gmail.com>
2023-01-04 12:13:41 +0200

    back to the commit 4199785b4e

--- atlo/main/models.py
+++ atlo/main/models.py
@@ -1,22 +1,21 @@
 from django.db import models
-from django.contrib.auth.models import User
-from django.conf import settings
 
-# class User(models.Model):
-#     name = models.CharField(max_length=200, null=True)
-#     email = models.EmailField(null=True)
 
-#     def __str__(self):
-#         return self.name
+class User(models.Model):
+    name = models.CharField(max_length=200, null=True)
+    email = models.EmailField(null=True)
 
-#     class Meta:
-#         verbose_name = "Site user"
-#         verbose_name_plural = "Site users"
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Site user"
+        verbose_name_plural = "Site users"
 
 
 class Traffic(models.Model):
-    # user = models.OneToOneField(User, on_delete=models.CASCADE)
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    # user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
     from_left = models.IntegerField(default=0, null=True)
     from_right = models.IntegerField(default=0, null=True)
     from_top = models.IntegerField(default=0, null=True)
