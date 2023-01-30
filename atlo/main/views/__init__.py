417f9e11328e3fe63d11dcc8abf683e6f5a5ab14
OleksandrNovitskyi <novitskyi.o.s@gmail.com>
2023-01-30 13:41:44 +0200

    spliting views

---
+++ atlo/main/views/__init__.py
@@ -0,0 +1,2 @@
+from .views import *
+from .log_reg_views import registerPage, loginPage, logoutUser
--- atlo/main/views/views.py
+++ atlo/main/views/views.py
@@ -1,62 +1,16 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.urls import reverse
-from django.contrib.auth import authenticate, login, logout
-from django.db import transaction
 
-from .forms import CreateUserForm, TrafficForm, SpeedForm
-from .models import Traffic, Results, Speed
-from . import logic
+from ..forms import TrafficForm, SpeedForm
+from ..models import Traffic, Results, Speed
+from .. import logic
 
 
 def index(request):
     user = request.user
     traffic = Traffic.objects.filter(user=user).last()
     return redirect(reverse("main:activate_traffic", args=[traffic.pk]))
-
-
-@transaction.atomic  # if something wrong - nofing save to DB
-def registerPage(request):
-    form = CreateUserForm()
-    form_traffic = TrafficForm()
-    if request.method == "POST":
-        form = CreateUserForm(request.POST)
-        form_traffic = TrafficForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            if form_traffic.is_valid():
-                traffic = form_traffic.save(commit=False)
-                traffic.user_id = user.id
-                traffic.save()
-                user = form.cleaned_data.get("username")
-                messages.success(request, "Account was created for " + user)
-                return redirect("main:login")
-    context = {"form": form, "form_traffic": form_traffic}
-    return render(request, "main/register.html", context)
-
-
-def loginPage(request):
-
-    if request.method == "POST":
-        username = request.POST.get("username")
-        email = request.POST.get("email")
-        password = request.POST.get("password")
-
-        user = authenticate(request, username=username, email=email, password=password)
-
-        if user is not None:
-            login(request, user)
-            return redirect("main:index")
-        else:
-            messages.info(request, "Username, email or password is wrong")
-
-    context = {}
-    return render(request, "main/login.html", context)
-
-
-def logoutUser(request):
-    logout(request)
-    return redirect("main:login")
 
 
 def addNewTraffic(request):
@@ -130,6 +84,7 @@ def activate_traffic(request, pk):
         if speed_form.is_valid():
             if "use_speed" in request.POST:
                 speed.speed = request.POST["speed"]
+                print(speed)
                 speed.save()
 
     time_l_r, time_t_b = logic.timing_traffic_lights(traffic)
